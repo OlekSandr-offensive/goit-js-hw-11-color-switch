53d8c9c334aaae1ebf8f520bcd19b40132566814
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-05-23 15:26:34 +0200

    перше завдання зроблене

--- index.js
+++ index.js
@@ -7,16 +7,35 @@ const colors = [
   '#795548',
 ];
 
-const startBtn = document.querySelector('button[data-modal-start]');
-const stopBtn = document.querySelector('button[data-modal-stop]');
+const refs = {
+  startBtn: document.querySelector('body button[data-action="start"]'),
+  stopBtn: document.querySelector('body button[data-action="stop"]'),
+  myBodyColor: document.querySelector('body'),
+};
+
+const PROM_DALEY = 1000;
 let timeoutId = null;
 
-startBtn.addEventListener('click', startColorBar);
+refs.startBtn.addEventListener('click', startTimerColor);
+refs.stopBtn.addEventListener('click', stopTimerColor);
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 function startColorBar() {
-  body.
+  const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  refs.myBodyColor.style.background = randomColor;
+  refs.startBtn.disabled = true;
+}
+
+function startTimerColor() {
+  timeoutId = setInterval(() => {
+    startColorBar();
+  }, PROM_DALEY);
+}
+
+function stopTimerColor() {
+  refs.startBtn.disabled = false;
+  clearInterval(timeoutId);
 }
